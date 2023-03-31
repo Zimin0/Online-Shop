1accab70242d0930ecfdab96ede5ef735d106b7d
Zimin0 <nikzim2004@gmail.com>
2023-03-31 22:24:06 +0300

    Added PostgreSQL

--- englishSite/settings.py
+++ englishSite/settings.py
@@ -86,12 +86,15 @@ WSGI_APPLICATION = 'englishSite.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'mine_project',
+'USER': 'postgres',
+'PASSWORD': '1',
+'HOST': 'localhost',
+'PORT': '5432'
 }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
